ab01b29dad5cba816a60bcfc32f5e40f2ca1bc5e
1B0v0 <javierazapata@liceovvh.cl>
2026-04-21 09:29:50 -0400

    pmare

---
+++ python/fundamentos/fundamentos/02_estructura_control/10_bucles_while.py
@@ -0,0 +1,16 @@
+while condicion:
+    #Código que se ejecuta mientras la condición se cumpla
+
+#while
+    num = 0
+while num < 4:
+    print("bucle while -", num)
+    num += 1
+#Imprime: bucle while - 0, bucle while - 1, bucle while - 2, bucle while - 3
+
+num = 0
+while num < 4:
+    print("bucle while -", num)
+    num += 1
+else:
+    print("Acabamos de salir del bucle")
