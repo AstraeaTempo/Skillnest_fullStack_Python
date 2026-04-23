ae1ef5e6614d0b105bec8d1b92687d3dba43e930
1B0v0 <javierazapata@liceovvh.cl>
2026-04-23 13:52:15 -0400

    f

--- python/Core/Funciones_intermedias_(Core)_javiera_2026-04-23/funciones_intermedia.py
+++ python/Core/Funciones_intermedias_(Core)_javiera_2026-04-23/funciones_intermedia.py
@@ -5,21 +5,53 @@ puntajes[1][0] = 600
 print(puntajes)
 
 
-# Lista de creadores de contenido en una plataforma de streaming
+# Lista de creadores de contenido en una plataforma de streaming--------------------------------------------
 streamers = [
     {"nombre": "GameNinjaPro", "seguidores": 250000},
     {"nombre": "PixelWarrior", "seguidores": 180000}
 ]
+# Actualización de datos
+streamers[0]["nombre"] = "EliteGamerX"
+print(streamers[0]["nombre"])
+
+#Funciones para recorrer y representar datos:
+def iterar_diccionario(lista):
+    return
 
 
-# Eventos en distintas ciudades del mundo
+# Eventos en distintas ciudades del mundo-------------------------------------------------------------------
 eventos = {
     "Estados Unidos": ["Los Ángeles", "Nueva York", "Las Vegas"],
     "España": ["Madrid", "Barcelona", "Valencia"]
 }
+# Actualización de datos
+eventos["Estados Unidos"][2] = "San Francisco"
+print(eventos["Estados Unidos"][2])
+
+#Mostrar información de un diccionario con listas:
+def mostrar_informacion(diccionario):
+    categorias = {
+    "juegos_populares": [
+        "Fortnite", 
+        "Minecraft", 
+        "Valorant", 
+        "GTA V",
+    ],
+    "ciudades_eventos": [
+        "Nueva York",
+        "Madrid",
+        "Tokio",
+    ]
+}
+    for item in diccionario:
+        return
 
 
-# Coordenadas de la sede de un torneo internacional
+
+# Coordenadas de la sede de un torneo internacional---------------------------------------------------------
 ubicacion = [
     {"latitud": 34.052235, "longitud": -118.243683}
 ]
+# Actualización de datos
+ubicacion[0]["latitud"] = 40.712776
+print(ubicacion[0]["latitud"])
