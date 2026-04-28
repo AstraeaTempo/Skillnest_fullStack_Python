1b6f2a841b6ff706120bd615523f8b72ed6d714c
1B0v0 <javierazapata@liceovvh.cl>
2026-04-28 12:58:10 -0400

    l

--- python/fundamentos/oop/4.-usuario_streaming.py
+++ python/fundamentos/oop/4.-usuario_streaming.py
@@ -8,30 +8,47 @@ class UsuarioStreaming:
 
 
     def agregar_a_lista(self, titulo):
-        """Agrega un contenido a la lista de reproducción del usuario."""
-        self.lista_reproduccion += titulo
+        self.lista_reproduccion(titulo)
+        print(f"Titulo '{titulo}' agregado correctamente")
 
 
     def ver_contenido(self, titulo):
-        """Simula que el usuario reproduce un contenido."""
-        self.nombre += titulo
+        if titulo in self.lista_reproduccion:
+            print(f"El usuario {self.nombre} esta viendo '{titulo}'")
+        else:
+            print(f"Titulo no encontrado {titulo}")
 
 
     def cambiar_suscripcion(self, nueva_suscripcion):
-        """Cambia el tipo de suscripción del usuario."""
-        self.suscripcion += nueva_suscripcion
+        susAntigua = self.suscripcion
+        self.suscripcion = nueva_suscripcion
+        print(f"Suscripción cambia de {susAntigua} a {nueva_suscripcion}")
 
 
     def mostrar_info_usuario(self):
-        """Muestra la información del usuario y su lista de reproducción."""
-        usuario += self
-
-miyano = UsuarioStreaming("Miyano", "miyagi@codingdojo.la", "Gratis")
-sasaki = UsuarioStreaming("Sasaki", "daniel@codingdojo.la", "Premium")
+        print(f"Nombre: {self.nombre}")
+        print(f"Email: {self.email}")
+        print(f"Suscripción: {self.suscripcion}")
+        if len(self.lista_reproduccion) == 0:
+            print(f"La lista de reproducción esta vacia")
+        else:
+            print(f"Lista de reproducción: \n- {"\n- ".join(self.lista_reproduccion)}")
 
 # Todos los valores que se deban registrar debe ser con input
 # Añadir un menu While para llamar a los métodos.
 # (Menú de selección)
+
+miyano = UsuarioStreaming("Miyano", "miyano@codingdojo.la")
+miyano.agregar_a_lista("La vida")
+miyano.cambiar_suscripcion("Premium")
+miyano.ver_contenido("ORV")
+miyano.mostrar_info_usuario()
+
+sasaki = UsuarioStreaming("Sasaki", "sasaki@codingdojo.la")
+sasaki.agregar_a_lista("Tu vida")
+sasaki.cambiar_suscripcion("Estandar")
+sasaki.ver_contenido("Hablame")
+sasaki.mostrar_info_usuario()
 
 #Menu
 continuar = True
@@ -41,13 +58,7 @@ while continuar:
     print("---2.- ejercicio 2---")
     print("---3.- ejercicio 3---")
     print("---4.- ejercicio 4---")
-    print("---5.- ejercicio 5---")
-    print("---6.- ejercicio 6---")
-    print("---7.- ejercicio 7---")
-    print("---8.- ejercicio 8---")
-    print("---9.- ejercicio 9---")
-    print("---10.- ejercicio 10---")
-    opcion = input("\n---- elige una opcion: (1-10) (0 para salir) = ")
+    opcion = input("\n---- elige una opcion: (1-4) (0 para salir) = ")
     if opcion == "1":
         print("\nEjecutando ejercicio 1: ")
         ()
@@ -59,24 +70,6 @@ while continuar:
         ()
     elif opcion == "4":
         print("\nEjecutando ejercicio 4: ")
-        ()
-    elif opcion == "5":
-        print("\nEjecutando ejercicio 5: ")
-        ()
-    elif opcion == "6":
-        print("\nEjecutando ejercicio 6: ")
-        ()
-    elif opcion == "7":
-        print("\nEjecutando ejercicio 7: ")
-        ()
-    elif opcion == "8":
-        print("\nEjecutando ejercicio 8: ")
-        ()
-    elif opcion == "9":
-        print("\nEjecutando ejercicio 9: ")
-        ()
-    elif opcion == "10":
-        print("\nEjecutando ejercicio 10: ")
         ()
     elif opcion == "0":
         print("Saliendo...")
